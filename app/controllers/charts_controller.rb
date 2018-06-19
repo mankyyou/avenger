8ce2cb3aa2063677baea793fff6436c07daa54e6
Pete <namtrung1810it.gmail.com>
2018-06-19 15:44:49 +0700

    Thong ke

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -18,11 +18,10 @@ class ChartsController < ApplicationController
       @month = @param.split("-")[1]
       @year = @param.split("-")[0]
       @orders = Order.where("EXTRACT(month FROM created_at) = ? AND EXTRACT(year FROM created_at) = ?", "#{@month}", "#{@year}")
-      # elsif @param === nil
-    # elsif @param.empty? === true
-    #   respond_to do |format|
-    #     format.html { redirect_to charts_index_path, notice: "Gear was successfully created." }
-    #   end
-    # end
+    elsif @param.empty? === true
+      respond_to do |format|
+        format.html { redirect_to charts_index_path, notice: "Gear was successfully created." }
+      end
+    end
   end
 end
