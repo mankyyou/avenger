fe649c6765e96a33d6621bac986eaa740110148a
Pete <namtrung1810it.gmail.com>
2018-07-03 08:25:18 +0700

    updatetrung10

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,10 +4,9 @@ class StoreController < ApplicationController
 
   def index
     @date1 = DateTime.now
-    # @date2 = DateTime.new(2018, 06, 24)
+    @date2 = DateTime.new(2018, 06, 24).strftime("%Y-%m-%d")
     # @date3 = (@date1 - @date2).to_i
     # @date3 = @date2.strftime("%m/%d/%Y")
-    @types = Type.all
     @brands = Brand.all
     if params[:query].present?
       @gears = Gear.search_name(params[:query]).paginate(page: params[:page], per_page: 7).order(created_at: :desc)
